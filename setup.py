20d2ceed17838afc6939ec31c52e501f4dcab6ec
Daniel Sali <daniel.sali		i += 1daniel.sali@alphacruncher.com>
2020-03-26 16:46:00 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='DatastreamDSWS',
+      version='1.0.3',
+      description='Python API Package for Refinitiv Datastream Webservices (DSWS)',
+      url='https://github.com/DatastreamDSWS/Datastream',
+      author='Vidya Dinesh',
+      author_email='datastream@refinitiv.com',
+      license='MIT',
+      packages=['DatastreamDSWS'],
+      zip_safe=False)
